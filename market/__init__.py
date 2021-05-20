de1d9f39d9fd856bd5233929a011a9ca79fee2cf
Shivanshu Sarin <shivanshusarin@gmail.com>
2021-05-20 15:13:06 +0530

    First Commit of this FLask APP

---
+++ market/__init__.py
@@ -0,0 +1,21 @@
+from enum import unique
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = '9258a94cec8479e5f741d241'
+
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login_page'
+login_manager.login_message_category = 'info'
+
+from market import routes
+
+
+
+
